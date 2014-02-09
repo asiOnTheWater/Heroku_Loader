5fa5a601d6c8e79f9952277d44009d62012f0bf4
ayonesauce <anthonysi@sendgrid.com>
2014-02-08 20:35:04 -0800

    changed endpoint to /

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 var PORT = Number(process.env.PORT || 9222);
 
-app.get('/endpoint', function(req, res) {
-  res.send("Hey", 200);
+app.get('/', function(req, res) {
+  res.send("Hey this is a Test", 200);
 });
 
 app.get('/loaderio-44003e740fcbcfdbf4d63612f8971ec8', function(req, res) {
